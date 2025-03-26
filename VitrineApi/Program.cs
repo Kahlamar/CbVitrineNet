7ed6a9459fdb9b329eba4fb5fbdace26df137227
Cyril Brun <brun.cyril@hotmail.fr>
2025-03-26 15:52:41 +0100

    uvegarde avant refactoring MongoDB

--- VitrineApi/Program.cs
+++ VitrineApi/Program.cs
@@ -4,31 +4,30 @@ using StackExchange.Redis;
 using VitrineApi.Services.CV;
 using MongoDB.Driver;
 
-namespace VitrineApi
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddScoped<IVitrineService, VitrineService>();
-            builder.Services.AddScoped<ICvService, CvService>();
-            builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("redis:6379"));
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            builder.Services.AddSingleton<IMongoClient>(sp =>
-            {
-                var connectionString = builder.Configuration.GetValue<string>("mongodb://root:example@mongodb/")
-                ?? "mongodb://root:example@mongodb/";
-                return new MongoClient(connectionString);
-            });
-            builder.Services.AddGraphQLServer();
+namespace VitrineApi;
 
-            var app = builder.Build();
-            app.MapControllers();
-            app.MapGet("/", () => "Démarrage Microservice OK");
-            app.Run();
-        }
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddScoped<IVitrineService, VitrineService>();
+        builder.Services.AddScoped<ICvService, CvService>();
+        builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("redis:6379"));
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        builder.Services.AddSingleton<IMongoClient>(sp =>
+        {
+            var connectionString = builder.Configuration.GetValue<string>("mongodb://root:example@mongodb/")
+            ?? "mongodb://root:example@mongodb/";
+            return new MongoClient(connectionString);
+        });
+        //builder.Services.AddGraphQLServer();
+
+        var app = builder.Build();
+        app.MapControllers();
+        app.MapGet("/", () => "Démarrage Microservice OK");
+        app.Run();
     }
 }
